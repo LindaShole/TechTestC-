eabdaf4a1187465674bacdd6aa7a0f1345d80067
VMuzi <wmavaido@gmail.com>
2023-08-08 20:42:00 +0200

    initial commit

---
+++ TechTest/AnyCompany.Core/AutoMapping.cs
@@ -0,0 +1,17 @@
+﻿using AnyCompany.Infrastructure.Repository.Model;
+using AnyCompany.Service.Customer.ViewModel;
+using AnyCompany.ViewModel;
+using AutoMapper;
+
+namespace AnyCompany.Core
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping() { 
+            
+            CreateMap<CustomerOrders, CustomerOrdersView>();
+            CreateMap<Order,OrderView>().ReverseMap();
+            CreateMap<Customer,CustomerView>().ReverseMap();
+        }
+    }
+}
